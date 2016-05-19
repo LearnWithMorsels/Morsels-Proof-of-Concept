2473a1729bd46a8477c75bfa2072b9276b0dc85c
Andrew Hosgood <andi@floor44.co.uk>
2016-05-19 23:15:59 +0100

    App init update

--- src/core/js/morsels.js
+++ src/core/js/morsels.js
@@ -3,7 +3,14 @@ import {CourseModel} from './models/course';
 
 var objConfig = new ConfigModel( '../morsels/app/config.json' );
 
-objConfig.promise.then( () => { console.log( objConfig.get() ) } );
-objConfig.promise.then( () => { console.log( objConfig.get( 'theme' ) ) } );
-
-CourseModel( 'en' ).then( ( data ) => { console.log( data ) } );
+objConfig.promise.then(
+    () => {
+        CourseModel( objConfig.get( 'languages' ).primary )
+            .then(
+                ( data ) => {
+                    console.log( data );
+                    console.log( 'Sections', data._sections );
+                }
+            );
+    }
+);
