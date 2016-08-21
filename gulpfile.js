ba5fd38fd6ed0cf68a9d9bc1a666718de3f244d3
Andrew Hosgood <andi@floor44.co.uk>
2016-08-22 00:36:57 +0100

    Removing static docs folder

--- gulpfile.js
+++ gulpfile.js
@@ -321,7 +321,7 @@ gulp.task( 'app:data', () => {
 
 gulp.task( 'docs', () => {
 	gulp.src( './src/core/js/**/*.js' )
-		.pipe( jsdoc( { opts: { destination: './docs/jsdocs/core/' } } ) );
+		.pipe( jsdoc( { opts: { destination: './docs/core/' } } ) );
 } );
 
 gulp.task( 'archive', () => {
