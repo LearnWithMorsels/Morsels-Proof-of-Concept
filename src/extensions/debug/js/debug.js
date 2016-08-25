f3eb2723e8b730bb4b3f5c9a65eccdafd38d4434
Andrew Hosgood <andi@floor44.co.uk>
2016-08-25 13:39:10 +0100

    Updates to debug, minor update to templates

--- src/extensions/debug/js/debug.js
+++ src/extensions/debug/js/debug.js
@@ -9,6 +9,7 @@ export default class extends Extension {
 		this.config = config;
 
 		this.showDebugHeader();
+		this.showStructure();
 
 	}
 
@@ -26,14 +27,88 @@ export default class extends Extension {
 
 			console.log.apply( console, args );
 		} catch( e ) {
-			if( window.console ) {
-				if( window.console.info ) {
-					console.info( 'LearnWithMorsels/Morsels' );
-				} else {
-					console.log( 'LearnWithMorsels/Morsels' );
-				}
+			if( window.console.info ) {
+				console.info( 'LearnWithMorsels/Morsels' );
+			} else {
+				console.log( 'LearnWithMorsels/Morsels' );
 			}
 		}
 
 	}
+
+	showStructure() {
+
+		let course = window.Morsels.app.children[0],
+			summary = {
+				course: {},
+				stacks: [],
+				cards: []
+			};
+
+		this.consoleGroupIfExists( 'COURSE: ' + course.properties.title );
+
+		summary.course.hasTitle = ( course.properties.title !== null && course.properties.title !== undefined );
+
+		this.consoleGroupIfExists( 'Properties', true );
+		window.console.log( course );
+		this.consoleGroupEndIfExists();
+
+		for( let intStack in course.children ) {
+			let stack = course.children[intStack];
+
+			summary.stacks[intStack] = {
+				hasTitle: ( stack.properties.title !== null && stack.properties.title !== undefined ),
+				hasTitle2: stack.properties.title ? '✔' : '✘'
+			};
+
+			this.consoleGroupIfExists( 'STACK: ' + stack.properties.title );
+
+			this.consoleGroupIfExists( 'Properties', true );
+			window.console.log( stack );
+			this.consoleGroupEndIfExists();
+
+			for( let intCard in stack.children ) {
+				let card = stack.children[intCard];
+
+				summary.stacks[intStack] = {
+					stack: intStack,
+					hasTitle: ( card.properties.title !== null && card.properties.title !== undefined )
+				};
+
+				this.consoleGroupIfExists( 'CARD: ' + card.properties.title, true );
+				window.console.log( card );
+				this.consoleGroupEndIfExists();
+			}
+
+			this.consoleGroupEndIfExists();
+		}
+
+		this.consoleGroupEndIfExists();
+
+		console.table( summary.course );
+		console.table( summary.stacks );
+		console.table( summary.cards );
+
+	}
+
+	consoleGroupIfExists( log, collapse ) {
+
+		if( window.console.group ) {
+			if( collapse === true ) {
+				window.console.groupCollapsed( log );
+			} else {
+				window.console.group( log );
+			}
+		}
+
+	}
+
+	consoleGroupEndIfExists() {
+
+		if( window.console.group ) {
+			window.console.groupEnd();
+		}
+
+	}
+
 }
